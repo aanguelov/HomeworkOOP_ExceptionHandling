019350038c7a89d163c42edeed04ccc60408f740
aanguelov <aanguelov@gmail.com>
2015-06-04 16:10:35 +0300

    02_EnterNumbers modified

--- HomeworkOOP_ExceptionHandling/02_EnterNumbers/Program.cs
+++ HomeworkOOP_ExceptionHandling/02_EnterNumbers/Program.cs
@@ -10,6 +10,41 @@ namespace _02_EnterNumbers
     {
         static void Main(string[] args)
         {
+            int previous = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                int inputNumber = ReadNumber(1, 100);
+
+                if (i > 0)
+                {
+                    if (!IsBigger(inputNumber,previous))
+                    {
+                        Console.WriteLine("Give it another try!");
+                        i--;
+                        inputNumber = previous;
+                    }
+                }
+                previous = inputNumber;
+            }
+        }
+
+        static int ReadNumber(int start, int end)
+        {
+            int number = int.Parse(Console.ReadLine());
+            if (number < start || number > end)
+            {
+                throw new ArgumentOutOfRangeException("Number not in range!");
+            }
+            return number;
+        }
+        static bool IsBigger(int current, int previous)
+        {
+            bool isBigger = false;
+            if (current > previous)
+            {
+                isBigger = true;
+            }
+            return isBigger;
         }
     }
 }
